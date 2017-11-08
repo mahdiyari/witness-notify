7601f258620d91c61b4a3b4d55aed16d51ef5735
Mahdi Yari <m.yary15@gmail.com>
2017-11-08 20:55:24 +0330

    Added Extra if() for checking Missed Blocks

--- cronjob-1minute.php
+++ cronjob-1minute.php
@@ -23,28 +23,30 @@ foreach($result as $data){ // Foreach All Witnesses from 1 to 100
 		$totalmiss = $y['totalmiss'];
 		$total_missed = $data->total_missed;
 		$last_confirmed_block_num = $data->last_confirmed_block_num;
-		if($y['userid'] != 0 && $y['userid'] != null && $y['userid'] != ''){ // For Mention in Discord
-			$userid = $y["userid"];
-		}else{
-			$userid = '';
-		}
-		if($y['username'] != null && $y['username'] != ''){ // For Mention in Steemit.chat
-			$uname = $y["username"];
-		}else{
-			$uname = '';
-		}
-		if($y['missed'] == 1){ // Checking If Witnesses Recovered
-			$lastconfirm = $y['lastconfirm'];
-			if($lastconfirm < $last_confirmed_block_num){
-				file_get_contents("$server?u=$userr&idd=$userid&type=2&block=$last_confirmed_block_num"); // Sending Recovered Message to Discord Channel
-				file_get_contents("$server?u=$userr&uname=$uname&type=2&block=$last_confirmed_block_num&id=2&miss=$total_missed"); // Sending Recovered Message to https://steemit.chat/channel/witness-blocks
-				$result = $conn->query("UPDATE `witnesses` SET `missed`=0,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'"); // Updating Database
+		if(is_numeric($totalmiss) && is_numeric($total_missed) && is_numeric($last_confirmed_block_num)){
+			if($y['userid'] != 0 && $y['userid'] != null && $y['userid'] != ''){ // For Mention in Discord
+				$userid = $y["userid"];
+			}else{
+				$userid = '';
 			}
-		}
-		if($totalmiss < $total_missed){ // Checking If Witnesses Missed
-			file_get_contents("$server?u=$userr&idd=$userid&type=1"); // Sending Missed Message to Discord Channel
-			file_get_contents("$server?u=$userr&id=2&type=1&uname=$uname&miss=$total_missed");  // Sending Missed Message to https://steemit.chat/channel/witness-blocks
-			$result = $conn->query("UPDATE `witnesses` SET `totalmiss`='$total_missed',`missed`=1,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'");//Updating Database
+			if($y['username'] != null && $y['username'] != ''){ // For Mention in Steemit.chat
+				$uname = $y["username"];
+			}else{
+				$uname = '';
+			}
+			if($y['missed'] == 1){ // Checking If Witnesses Recovered
+				$lastconfirm = $y['lastconfirm'];
+				if($lastconfirm < $last_confirmed_block_num){
+					file_get_contents("$server?u=$userr&idd=$userid&type=2&block=$last_confirmed_block_num"); // Sending Recovered Message to Discord Channel
+					file_get_contents("$server?u=$userr&uname=$uname&type=2&block=$last_confirmed_block_num&id=2&miss=$total_missed"); // Sending Recovered Message to https://steemit.chat/channel/witness-blocks
+					$result = $conn->query("UPDATE `witnesses` SET `missed`=0,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'"); // Updating Database
+				}
+			}
+			if($totalmiss < $total_missed){ // Checking If Witnesses Missed
+				file_get_contents("$server?u=$userr&idd=$userid&type=1"); // Sending Missed Message to Discord Channel
+				file_get_contents("$server?u=$userr&id=2&type=1&uname=$uname&miss=$total_missed");  // Sending Missed Message to https://steemit.chat/channel/witness-blocks
+				$result = $conn->query("UPDATE `witnesses` SET `totalmiss`='$total_missed',`missed`=1,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'");//Updating Database
+			}
 		}
 	}
 }
@@ -66,28 +68,30 @@ foreach($result as $data){
 		$totalmiss = $y['totalmiss'];
 		$total_missed = $data->total_missed;
 		$last_confirmed_block_num = $data->last_confirmed_block_num;
-		if($y['userid'] != 0 && $y['userid'] != null && $y['userid'] != ''){
-			$userid = $y["userid"];
-		}else{
-			$userid = '';
-		}
-		if($y['username'] != null && $y['username'] != ''){
-			$uname = $y["username"];
-		}else{
-			$uname = '';
-		}
-		if($y['missed'] == 1){ // Checking If Witnesses Recovered
-			$lastconfirm = $y['lastconfirm'];
-			if($lastconfirm < $last_confirmed_block_num){
-				file_get_contents("$server?u=$userr&idd=$userid&type=2&block=$last_confirmed_block_num"); // Sending Recovered Message to Discord Channel
-				file_get_contents("$server?u=$userr&uname=$uname&type=2&block=$last_confirmed_block_num&id=2&miss=$total_missed"); // Sending Recovered Message to https://steemit.chat/channel/witness-blocks
-				$result = $conn->query("UPDATE `witnesses` SET `missed`=0,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'"); //Updating Database
+		if(is_numeric($totalmiss) && is_numeric($total_missed) && is_numeric($last_confirmed_block_num)){
+			if($y['userid'] != 0 && $y['userid'] != null && $y['userid'] != ''){
+				$userid = $y["userid"];
+			}else{
+				$userid = '';
 			}
-		}
-		if($totalmiss < $total_missed){ // Checking If Witnesses Missed
-			file_get_contents("$server?u=$userr&idd=$userid&type=1"); // Sending Missed Message to Discord Channel
-			file_get_contents("$server?u=$userr&id=2&type=1&uname=$uname&miss=$total_missed"); // Sending Missed Message to https://steemit.chat/channel/witness-blocks
-			$result = $conn->query("UPDATE `witnesses` SET `totalmiss`='$total_missed',`missed`=1,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'"); //Updating Database
+			if($y['username'] != null && $y['username'] != ''){
+				$uname = $y["username"];
+			}else{
+				$uname = '';
+			}
+			if($y['missed'] == 1){ // Checking If Witnesses Recovered
+				$lastconfirm = $y['lastconfirm'];
+				if($lastconfirm < $last_confirmed_block_num){
+					file_get_contents("$server?u=$userr&idd=$userid&type=2&block=$last_confirmed_block_num"); // Sending Recovered Message to Discord Channel
+					file_get_contents("$server?u=$userr&uname=$uname&type=2&block=$last_confirmed_block_num&id=2&miss=$total_missed"); // Sending Recovered Message to https://steemit.chat/channel/witness-blocks
+					$result = $conn->query("UPDATE `witnesses` SET `missed`=0,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'"); //Updating Database
+				}
+			}
+			if($totalmiss < $total_missed){ // Checking If Witnesses Missed
+				file_get_contents("$server?u=$userr&idd=$userid&type=1"); // Sending Missed Message to Discord Channel
+				file_get_contents("$server?u=$userr&id=2&type=1&uname=$uname&miss=$total_missed"); // Sending Missed Message to https://steemit.chat/channel/witness-blocks
+				$result = $conn->query("UPDATE `witnesses` SET `totalmiss`='$total_missed',`missed`=1,`lastconfirm`='$last_confirmed_block_num' WHERE `user`='$userr'"); //Updating Database
+			}
 		}
 	}
 }
